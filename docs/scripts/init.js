ab331407c9d6cd04fbec5e716ef09afe0237d667
Alexander Bazo <alexanderbazo@googlemail.com>
2019-10-17 16:28:12 +0200

    Show page revision date in readable format (DE)

--- docs/scripts/init.js
+++ docs/scripts/init.js
@@ -1,2 +1,28 @@
-// Fix link to start page
-$(".navbar-brand").attr("href", "/mme/");
+(function() {
+
+	const WEEKDAYS = ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+	MONTHNAMES = ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezemeber"];
+
+    function initialize() {
+        setHomepageLink();
+        convertRevisionDate();
+    }
+
+    // Fix link to start page
+    function setHomepageLink(path) {
+        $(".navbar-brand").attr("href", "/mme/");
+    }
+
+    function convertRevisionDate() {
+        let dateEl = document.querySelector(".updated_at .date"),
+            date = new Date(dateEl.innerHTML),
+            day = WEEKDAYS[date.getDay()],
+            month = MONTHNAMES[date.getMonth()],
+            dateText = `${day}, ${date.getDate()}. ${month} ${date.getFullYear()}`;
+        dateEl.innerHTML = dateText;
+    }
+
+    // Call initialize() when page is ready
+    window.onload = initialize;
+
+}());
